3c646dda3f789152ced8046b637fb92c648a406b
woml <1096461471@qq.com>
2020-09-25 19:56:52 +0800

    Default Changelist

---
+++ Dist.h
@@ -0,0 +1,15 @@
+//
+// Created by 党鸿博 on 2020/9/25.
+//
+
+#ifndef DATASTRUCTION_DIST_H
+#define DATASTRUCTION_DIST_H
+
+class Dist {
+public:
+    int index;
+    double length;
+    int pre;
+};
+
+#endif //DATASTRUCTION_DIST_H
